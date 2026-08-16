6361406a2327f44874577abb10e621072bc4ace9
kalkidan404 <kalrokal1621@gmail.com>
2026-08-16 12:28:41 +0300

    prepare backend for deployment

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { prisma } = require("../config/prisma");
 
-const { generateToken } = require("../utils/jwt");
+const { generateToken } = require("../config/jwt");
 const register = async (req, res) => {
     try {
         const { name, email, password } = req.body;
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-const { verifyToken } = require("../utils/jwt");
+const { verifyToken } = require("../config/jwt");
 
 const authMiddleware = (req, res, next) => {
     try {
