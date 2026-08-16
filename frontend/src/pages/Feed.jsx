e3dfd62d78f1218ca7b9ac5985bc725d33329da0
kalkidan404 <kalrokal1621@gmail.com>
2026-08-16 20:16:54 +0300

    name edits for import

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import PostCard from "../components/PostCard";
 import { apiRequest } from "../api/api";
 
--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar";
 import UserCard from "../components/UserCard";
 import { apiRequest } from "../api/api";
 
